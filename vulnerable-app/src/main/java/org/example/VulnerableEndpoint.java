e0e29b6a70be3163dc7566315bd8f6c8e5cf6a51
nguy11 <duc11.nguyenvan@nttdata.com>
2021-12-16 02:15:38 +0700

    Add ThreadContext attack

--- vulnerable-app/src/main/java/org/example/VulnerableEndpoint.java
+++ vulnerable-app/src/main/java/org/example/VulnerableEndpoint.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +17,17 @@ public class VulnerableEndpoint {
     private static final Logger LOGGER = LogManager.getLogger(VulnerableEndpoint.class);
 
     @GetMapping("/")
-    public String vulnerable(@RequestBody(required = false) String payloadBody,
+    public String loggerVulnerable(@RequestBody(required = false) String payloadBody,
                              @RequestHeader(required = false, value = "X-Api-Version") String payloadHeader) {
         LOGGER.info("Received Request with body " + payloadBody);
         LOGGER.error("Received Request with header  " + payloadHeader);
         return "You have accessed a vulnerable Endpoint!!!";
     }
 
+    @GetMapping("/thread-context")
+    public String threadContextVulnerable(@RequestBody(required = false) String payloadBody) {
+        ThreadContext.put("payloadbody", payloadBody);
+        LOGGER.info("Received request for thread context vulnerable");
+        return "You have accessed a vulnerable Endpoint!!!";
+    }
 }
