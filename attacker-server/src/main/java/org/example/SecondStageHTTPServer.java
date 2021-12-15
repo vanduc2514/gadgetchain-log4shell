7f80286ef8fdc5c13ddd993f49809049f25b43f5
vanduc2514 <vanduc2514@gmail.com>
2021-12-16 01:56:53 +0700

    Hard code server address value

--- attacker-server/src/main/java/org/example/SecondStageHTTPServer.java
+++ attacker-server/src/main/java/org/example/SecondStageHTTPServer.java
@@ -1,4 +1,4 @@
-package com.example;
+package org.example;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
--- vulnerable-app/src/main/java/org/example/VulnerableEndpoint.java
+++ vulnerable-app/src/main/java/org/example/VulnerableEndpoint.java
@@ -1,11 +1,14 @@
-package com.example;
+package org.example;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.text.MessageFormat;
 
 @RestController
 public class VulnerableEndpoint {
